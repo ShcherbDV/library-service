ab4eb920bfd23c002ca7315f394ba4c5e29e6a4f
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-11-05 22:30:21 +0100

    Update library_service/urls.py -> add route for books

--- books/models.py
+++ books/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 Cover = models.TextChoices("Cover", "HARD SOFT")
 
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
