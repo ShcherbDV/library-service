716d2387c9ebd16d9edd611d311214617e03129e
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-11-11 17:11:34 +0100

    Update urls.py -> add path for urls app

--- library_service/urls.py
+++ library_service/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("", include("books.urls", namespace="books")),
     path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include("user.urls", namespace="user")),
 ]
