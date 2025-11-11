69aa256011412e48693979959a8e1b1b7b7ff1cf
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-11-11 22:52:38 +0100

    Update borrowings/views.py

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,12 +1,17 @@
 from rest_framework import generics
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer
 
 
-class BorrowingListView(generics.ListAPIView):
+class BorrowingListView(generics.ListCreateAPIView):
     queryset = Borrowing.objects.all()
-    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return BorrowingCreateSerializer
+        return BorrowingSerializer
+
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
