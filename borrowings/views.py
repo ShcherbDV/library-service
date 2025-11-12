67c18e71466ee1db844a7dd537d1a19dcb0f55eb
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-11-12 20:23:52 +0100

    Update borrowings/views.py -> add filtering & permissions

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,16 +1,40 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer
 
 
 class BorrowingListView(generics.ListCreateAPIView):
-    queryset = Borrowing.objects.all()
+
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.request.method == "POST":
             return BorrowingCreateSerializer
         return BorrowingSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Borrowing.objects.all()
+
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
+        else:
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user__id=user_id)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date=False)
+
+
+
+        return queryset.order_by("-borrow_date")
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -19,3 +43,4 @@ class BorrowingListView(generics.ListCreateAPIView):
 class BorrowingDetailView(generics.RetrieveAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
