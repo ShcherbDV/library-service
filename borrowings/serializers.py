4f00d7f94c31bde57a51bf0cc800f38d640e3532
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-11-12 21:26:36 +0100

    Update borrowings/serializer.py -> add borrowingReturnSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -13,7 +13,7 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "book",
             "user",
-            "is_active"
+            "is_active",
         )
         read_only_fields = ["borrow_date", "user", "is_active"]
 
@@ -38,3 +38,33 @@ class BorrowingCreateSerializer(BorrowingSerializer):
 
         borrowing = Borrowing.objects.create(user=request.user, **validated_data)
         return borrowing
+
+
+class BorrowingReturnSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+            "is_active",
+        )
+        read_only_fields = [
+            "borrow_date",
+            "user",
+            "is_active",
+            "id",
+            "book",
+            "is_active",
+            "expected_return_date",
+            "actual_return_date",
+        ]
+
+        def validate(self, attrs):
+            borrowing = self.instance
+            if borrowing.actual_return_date is not None:
+                raise serializers.ValidationError("This borrowing has already been returned")
+            return attrs
